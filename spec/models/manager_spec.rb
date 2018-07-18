f515286fa1f51e0c58b20794aadccf5a3f7ecd5b
Igor <igor.kotsiuba@bytebrand.net>
2018-07-19 00:32:25 +0300

    Add Rspec configuration and basic tests

--- spec/models/manager_spec.rb
+++ spec/models/manager_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Manager, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:tickets).with_foreign_key(:owner_id) }
 end
