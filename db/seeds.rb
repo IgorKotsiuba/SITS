367329187e5b32b9e0831a258232e1ff4cc65dd8
Igor <igor.kotsiuba@bytebrand.net>
2018-07-18 23:39:55 +0300

    Create manager model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+manager_data = [{ username: 'Riki', password: '123456789' }, { username: 'Bill', password: '123456789' }]
+manager_data.each do |manager|
+  Manager.create(username: manager[:username], password: manager[:password])
+end
---
+++ spec/factories/managers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :manager do
+    username        'riki'
+    password_digest '123456789'
+  end
+end
